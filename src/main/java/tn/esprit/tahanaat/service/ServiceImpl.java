150d2b153d5915cb8ae62e014ed514f1987e0034
TahaNaat <naat.taha@esprit.tn>
2024-12-09 16:37:01 +0100

    new methods

--- src/main/java/tn/esprit/tahanaat/service/ServiceImpl.java
+++ src/main/java/tn/esprit/tahanaat/service/ServiceImpl.java
@@ -1,6 +1,8 @@
 package tn.esprit.tahanaat.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import tn.esprit.tahanaat.entity.UserTest;
 import tn.esprit.tahanaat.repo.UserTestRepo;
@@ -9,11 +11,17 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ServiceImpl implements IService {
     UserTestRepo userTestRepo;
 
     @Override
     public UserTest ajouterUser(UserTest userTest) {
         return userTestRepo.save(userTest);
+    }
+   @Scheduled(cron = "*/60 * * * * *")//cron = "second minute hour day month year"
+   //@Scheduled(fixedDelay = 60000)//ms
+    void testScheduler(){
+        log.info("affichage test scheduler");
     }
 }
