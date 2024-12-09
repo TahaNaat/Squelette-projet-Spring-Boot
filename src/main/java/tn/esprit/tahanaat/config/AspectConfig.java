7effca465b2f34514f34c84bfb71cb17644bec6f
TahaNaat <naat.taha@esprit.tn>
2024-12-09 15:51:30 +0100

    minor tweaks

--- src/main/java/tn/esprit/tahanaat/config/AspectConfig.java
+++ src/main/java/tn/esprit/tahanaat/config/AspectConfig.java
@@ -2,6 +2,8 @@ package tn.esprit.tahanaat.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AspectConfig {
     // Ajouter ici les méthodes de configuration des aspects
-    @Before("execution(* tn.esprit.tahanaat.service.*.*(..))")
+    @AfterReturning("execution(* tn.esprit.tahanaat.service.*.ajouter*(..))")//etoile si type de return non specifiee ; void si pas de return
     void journaliser(JoinPoint joinPoint) {
-        log.info("Méthode invoquée : " + joinPoint.getSignature().getName());
+        log.info("C'est une methode d'ajout : " + joinPoint.getSignature().getName());
+    }
+    @AfterReturning("execution(* tn.esprit.tahanaat.service.*.affecter*(..))")
+    void journaliser1(JoinPoint joinPoint) {
+        log.info("C'est une methode d'ajout : " + joinPoint.getSignature().getName());
     }
 }
