2a60e10da527164d5aed5654c6b7dc9d551331c6
TahaNaat <naat.taha@esprit.tn>
2024-12-09 16:00:35 +0100

    added execution time calculation method

--- src/main/java/tn/esprit/tahanaat/config/AspectConfig.java
+++ src/main/java/tn/esprit/tahanaat/config/AspectConfig.java
@@ -2,11 +2,12 @@ package tn.esprit.tahanaat.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Aspect
 @Component
@@ -21,4 +22,13 @@ public class AspectConfig {
     void journaliser1(JoinPoint joinPoint) {
         log.info("C'est une methode d'ajout : " + joinPoint.getSignature().getName());
     }
+    @Around("execution(* tn.esprit.tahanaat.service.*.affecter*(..))")//calculer le temps d'execution
+    Object calculerTempsExecution(ProceedingJoinPoint pjp ) throws Throwable{
+        LocalDateTime debut = LocalDateTime.now();
+        Object obj = pjp.proceed();
+        LocalDateTime fin = LocalDateTime.now();
+        long duree = java.time.Duration.between(debut, fin).toMillis();
+        log.info("Temps d'execution de la methode : " + pjp.getSignature().getName() + " est " + duree + " millisecondes");
+        return obj;
+     }
 }
